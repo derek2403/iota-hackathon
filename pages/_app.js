f99b741907a887b82c0d559594eeec3f730754c2
Cedric <cedricctf11a@gmail.com>
2025-07-19 12:56:57 +0800

    setup dappkit provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import "@/styles/globals.css";
+import { createNetworkConfig, IotaClientProvider, WalletProvider } from '@iota/dapp-kit';
+import { getFullnodeUrl } from '@iota/iota-sdk/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Config options for the networks you want to connect to
+const { networkConfig } = createNetworkConfig({
+    localnet: { url: getFullnodeUrl('localnet') },
+    testnet: { url: getFullnodeUrl('testnet') },
+});
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <IotaClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <WalletProvider>
+          <Component {...pageProps} />
+        </WalletProvider>
+      </IotaClientProvider>
+    </QueryClientProvider>
+  );
 }
