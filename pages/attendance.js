3b945f8d8211ec6a38fca2ec9e1f65b751d57877
derek2403 <derekliew0@gmail.com>
2025-07-20 05:17:21 +0800

    created attendance page with verification

---
+++ pages/attendance.js
@@ -0,0 +1,9 @@
+import AttendanceVerification from '../components/AttendanceVerification';
+
+export default function Attendance() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8 px-4">
+      <AttendanceVerification />
+    </div>
+  );
+}
